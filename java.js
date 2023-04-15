589effdea17f307cb3bbec7bd1c429b2c67107f8
Gunasheelan A <127470100+gunasheelan1624@users.noreply.github.com>
2023-04-15 19:07:05 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,47 @@
+let resume = {
+    "details": {
+        "Name":  "Gunasheelan A",
+        "email": "gunasheelan1624@gmail.com",
+        "phone":  9342575974,
+        "myLocation": {
+            "country":  "India",
+            "state":    "tamilnadu",
+            "district":  "Coimbatore",
+             "address":  "4/44 First Lane lakshmi nagar kinathukadavoo 642109",
+    }
+    },
+    "profiles":[ 
+      {
+        "twitter":" ",
+        "instagram":" ",
+        "github":" ",
+      }
+     ],
+    "Education": [ 
+      {
+        "Kg to 10th":"New Bharath Matric. Hr. Sec. SCHOOL",
+        "10 to 12th":"New merit Matric. Hr. Sec. SCHOOL",
+        "Collage":"Hindusthan Institute of Technology",
+        "department":"Information Technology",
+      "Year of study":"2018 to 2022",
+        "cgpa":7,
+    }
+    ],
+    "Collage project":[
+        {
+        "Project name":"Real Time Drowsiness Detection using oper cv",
+        }
+    ],
+    "languages know": [
+        {
+        "First language":"Tamil",
+        "Second language":"English",
+    }
+    ], 
+    "Hobby": [
+     {
+        "name":"Playing games, reading book, typing programes",
+     }
+    ]
+   }
+console.log(resume); 
